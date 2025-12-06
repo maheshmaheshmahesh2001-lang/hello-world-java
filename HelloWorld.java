566ec64bc8de539b5dec09aeb83af54669525825
maheshmaheshmahesh2001-lang <maheshmaheshmahesh2001@gmail.com>
2025-12-06 12:11:42 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,8 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
     System.out.println("this is madhu");
-    System.out.println("Hello, canada");
-    System.out.println("Hello, kadapa ");
-    System.out.println("Hello, wed......................");
+
+    System.out.println("this is mahesh");
+   
   }
 }
